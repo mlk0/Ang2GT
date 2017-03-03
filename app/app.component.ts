cb8165df1d0b4160d03152dadc77135c3d88770b
Mirko Atanasov <mirko.atanasov@gmail.com>
2017-03-03 04:22:30 -0500

    ''

---
+++ app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'pm-app',
+    template: `
+        <h1>Angular2: Getting Started</h1>
+        van tu tri!
+    `
+})
+export class AppComponent { }
