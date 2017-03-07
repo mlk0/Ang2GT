2234532e9cc9b1fe253c3ab84d8fea36b06fbca7
Mirko Atanasov <mirko.atanasov@gmail.com>
2017-03-07 01:29:56 -0500

    play with packages and app.componet

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
-
+import {Component} from "@angular/core"
 @Component({
-    selector: 'pm-app',
-    template: `
-        <h1>Angular2: Getting Started</h1>
-
-        hi bro>, yo yo>>
+    selector : "pm-app",
+    template:`
+    <div>
+    <h1>{{pageTitle}}</h1>
+    my app component
+    2
+    
+    </div>
     `
 })
-export class AppComponent { }
+export class AppComponent{
+    pageTitle : string = "Acme Product Management";
+
+}
